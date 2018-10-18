6e2ee6735fe1bf4dd5d3ae2781781a2b554c0d2b
Raynaldo Rivera <royal.r4stl1n@gmail.com>
2018-10-18 02:20:53 -0700

    Updated initial starting tab

--- deriui.py
+++ deriui.py
@@ -366,7 +366,7 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
         self.orderTabWidget.setCurrentIndex(0)
-        self.tabWidget.setCurrentIndex(3)
+        self.tabWidget.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
